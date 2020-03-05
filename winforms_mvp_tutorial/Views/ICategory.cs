de26db62df5ede8f35783dd6b4e2a8654c64c9e4
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-03-05 23:47:48 +0200

    Added Views(Interfaces)

---
+++ winforms_mvp_tutorial/Views/ICategory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace winforms_mvp_tutorial.Views
+{
+    interface ICategory
+    {
+    }
+}
