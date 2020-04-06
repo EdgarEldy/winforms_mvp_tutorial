7c88611a1b95a552d1f2e076649c6f29665d234f
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-04-06 22:52:37 +0200

    Added option and details users forms

---
+++ winforms_mvp_tutorial/Modules/UsersModule/FormUsersOption.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winforms_mvp_tutorial.Modules.UsersModule
+{
+    public partial class FormUsersOption : MetroFramework.Forms.MetroForm
+    {
+        public FormUsersOption()
+        {
+            InitializeComponent();
+        }
+    }
+}
