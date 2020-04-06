c8e169c476760fce8ee7ddc1b99e1346e1198009
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-04-06 22:53:01 +0200

    Added a login form

--- winforms_mvp_tutorial/Modules/UsersModule/FormLogin.cs
+++ winforms_mvp_tutorial/Modules/UsersModule/FormLogin.cs
@@ -7,14 +7,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using winforms_mvp_tutorial.Presenters;
+using winforms_mvp_tutorial.Views;
 
 namespace winforms_mvp_tutorial.Modules.UsersModule
 {
-    public partial class FormLogin : Form
+    public partial class FormLogin : MetroFramework.Forms.MetroForm,IUser
     {
+        UserPresenter userp= new UserPresenter();
         public FormLogin()
         {
             InitializeComponent();
+            tbxUsername.Focus();
+        }
+
+        string IUser.tbxPwd
+        {
+            get { return tbxPwd.Text; }
+
+            set { tbxPwd.Text = value; }
+        }
+
+        string IUser.tbxUsername
+        {
+            get { return tbxUsername.Text; }
+
+            set { tbxUsername.Text = value; }
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            if (UserPresenter.Instance.Login(this))
+            {
+                var d = new Dashboard();
+                d.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Impossible de se connecter");
+                tbxUsername.Clear();
+                tbxPwd.Clear();
+                tbxUsername.Focus();
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
