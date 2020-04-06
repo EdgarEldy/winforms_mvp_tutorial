f3c340eee8338c62dc75f7ffdfb3618ce15c20ba
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-04-06 22:54:57 +0200

    Added an insert method to save a new category

--- winforms_mvp_tutorial/Presenters/CategoryPresenter.cs
+++ winforms_mvp_tutorial/Presenters/CategoryPresenter.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using winforms_mvp_tutorial.Models;
+using winforms_mvp_tutorial.Views;
 
 namespace winforms_mvp_tutorial.Presenters
 {
     class CategoryPresenter
     {
+        simple_store_dbEntities db = new simple_store_dbEntities();
+        private ICategory ICat;
+        category cat =new category();
+
+        public CategoryPresenter()
+        {
+            
+        }
+
+        #region Singleton
+
+        private static CategoryPresenter _instance;
+
+        public static CategoryPresenter Instance => _instance ?? (_instance = new CategoryPresenter());
+
+        #endregion
+
+        public void insertCategory(ICategory view)
+        {
+            ICat = view;
+            cat.cat_name = ICat.tbxCatName;
+            db.categories.Add(cat);
+            db.SaveChanges();
+        }
     }
 }
