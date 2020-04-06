15db10a7c391a176cdf61a62613e0ca3e65e14ad
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-04-06 22:55:58 +0200

    Added ICategory properties

--- winforms_mvp_tutorial/Views/ICategory.cs
+++ winforms_mvp_tutorial/Views/ICategory.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace winforms_mvp_tutorial.Views
 {
-    interface ICategory
+   public interface ICategory
     {
+        string tbxCatName { get; set; }
+        DataGridView dgvCategoriesDetails { get; }
+        ComboBox cbxCategories { get; }
     }
 }
