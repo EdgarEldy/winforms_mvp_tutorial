0e87e765bd3285b4c96fe2c415e46330b81cbf10
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-05-25 00:07:31 +0200

    Added ICategory members

--- winforms_mvp_tutorial/Modules/CategoriesModule/FormCategoriesDetails.cs
+++ winforms_mvp_tutorial/Modules/CategoriesModule/FormCategoriesDetails.cs
@@ -7,14 +7,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using winforms_mvp_tutorial.Views;
 
 namespace winforms_mvp_tutorial.Modules.CategoriesModule
 {
-    public partial class FormCategoriesDetails : MetroFramework.Forms.MetroForm
+    public partial class FormCategoriesDetails : MetroFramework.Forms.MetroForm,  ICategory
     {
         public FormCategoriesDetails()
         {
             InitializeComponent();
+        }
+
+        public ComboBox cbxCategories
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string tbxCatName
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        DataGridView ICategory.dgvCategoriesDetails
+        {
+            get { return dgvCategoriesDetails; }
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            FormCategoriesOption.Instance.Show();
         }
     }
 }
