34ef6dbffbae1dc7a2c116b1f659ec99a7fd2820
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-09-19 22:54:28 +0200

    Update categories option design

--- winforms_mvp_tutorial/Modules/CategoriesModule/FormCategoriesOption.Designer.cs
+++ winforms_mvp_tutorial/Modules/CategoriesModule/FormCategoriesOption.Designer.cs
@@ -41,12 +41,13 @@
             // 
             // btnClose
             // 
-            this.btnClose.Location = new System.Drawing.Point(197, 307);
+            this.btnClose.Location = new System.Drawing.Point(263, 281);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(75, 23);
             this.btnClose.TabIndex = 0;
             this.btnClose.Text = "Close";
             this.btnClose.UseSelectable = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
             // groupBox1
             // 
@@ -54,7 +55,7 @@
             this.groupBox1.Controls.Add(this.metroLabel1);
             this.groupBox1.Location = new System.Drawing.Point(23, 82);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(249, 118);
+            this.groupBox1.Size = new System.Drawing.Size(305, 98);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Category";
@@ -65,7 +66,7 @@
             // 
             // 
             this.tbxCatName.CustomButton.Image = null;
-            this.tbxCatName.CustomButton.Location = new System.Drawing.Point(53, 1);
+            this.tbxCatName.CustomButton.Location = new System.Drawing.Point(129, 1);
             this.tbxCatName.CustomButton.Name = "";
             this.tbxCatName.CustomButton.Size = new System.Drawing.Size(21, 21);
             this.tbxCatName.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
@@ -74,7 +75,7 @@
             this.tbxCatName.CustomButton.UseSelectable = true;
             this.tbxCatName.CustomButton.Visible = false;
             this.tbxCatName.Lines = new string[0];
-            this.tbxCatName.Location = new System.Drawing.Point(148, 45);
+            this.tbxCatName.Location = new System.Drawing.Point(133, 36);
             this.tbxCatName.MaxLength = 32767;
             this.tbxCatName.Name = "tbxCatName";
             this.tbxCatName.PasswordChar = '\0';
@@ -83,7 +84,7 @@
             this.tbxCatName.SelectionLength = 0;
             this.tbxCatName.SelectionStart = 0;
             this.tbxCatName.ShortcutsEnabled = true;
-            this.tbxCatName.Size = new System.Drawing.Size(75, 23);
+            this.tbxCatName.Size = new System.Drawing.Size(151, 23);
             this.tbxCatName.TabIndex = 1;
             this.tbxCatName.UseSelectable = true;
             this.tbxCatName.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
@@ -92,7 +93,7 @@
             // metroLabel1
             // 
             this.metroLabel1.AutoSize = true;
-            this.metroLabel1.Location = new System.Drawing.Point(19, 45);
+            this.metroLabel1.Location = new System.Drawing.Point(19, 36);
             this.metroLabel1.Name = "metroLabel1";
             this.metroLabel1.Size = new System.Drawing.Size(108, 19);
             this.metroLabel1.TabIndex = 0;
@@ -102,9 +103,9 @@
             // 
             this.groupBox2.Controls.Add(this.btnReset);
             this.groupBox2.Controls.Add(this.btnSave);
-            this.groupBox2.Location = new System.Drawing.Point(23, 217);
+            this.groupBox2.Location = new System.Drawing.Point(23, 198);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(249, 66);
+            this.groupBox2.Size = new System.Drawing.Size(221, 61);
             this.groupBox2.TabIndex = 2;
             this.groupBox2.TabStop = false;
             // 
@@ -125,12 +126,13 @@
             this.btnSave.TabIndex = 0;
             this.btnSave.Text = "Save";
             this.btnSave.UseSelectable = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // FormCategoriesOption
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(301, 347);
+            this.ClientSize = new System.Drawing.Size(350, 316);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.btnClose);
