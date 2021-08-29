619729d11b17a2f47a98a4de655baac222b1642e
Edgar Eldy <ed.eldy21@live.com>
2021-08-29 20:18:45 +0200

    Setting data annotations

--- winforms_mvp_tutorial/Models/Category.cs
+++ winforms_mvp_tutorial/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -19,6 +20,8 @@ namespace winforms_mvp_tutorial.Models
         // Properties
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(50)]
         public string CategoryName { get; set; }
     }
 }
