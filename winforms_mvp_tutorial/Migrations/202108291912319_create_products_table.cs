eac7beb535fb303ed5a4a73856888dd8b4e60010
Edgar Eldy <ed.eldy21@live.com>
2021-08-29 21:13:09 +0200

    Add products migration

---
+++ winforms_mvp_tutorial/Migrations/202108291912319_create_products_table.cs
@@ -0,0 +1,16 @@
+﻿namespace winforms_mvp_tutorial.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class create_products_table : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
