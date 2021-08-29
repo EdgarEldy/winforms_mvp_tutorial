9c77907ea2ce0fd35c4325eb990af2ac82ce58ca
Edgar Eldy <ed.eldy21@live.com>
2021-08-29 20:17:55 +0200

    Update Product Model : Add Properties

--- winforms_mvp_tutorial/Models/Product.cs
+++ winforms_mvp_tutorial/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -15,5 +16,22 @@ namespace winforms_mvp_tutorial.Models
         {
 
         }
+
+        // Properties
+        public int Id { get; set; }
+
+        public int? CategoryId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string ProductName { get; set; }
+
+        public double UnitPrice { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public virtual Category Category { get; set; }
     }
 }
