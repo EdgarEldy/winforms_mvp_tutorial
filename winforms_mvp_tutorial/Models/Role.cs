da387963d26f5c02e3048bba68b62ac5748e19c8
Edgar Eldy <ed.eldy21@live.com>
2021-08-29 20:35:50 +0200

    Update Role Model : Add properties

--- winforms_mvp_tutorial/Models/Role.cs
+++ winforms_mvp_tutorial/Models/Role.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -15,5 +16,16 @@ namespace winforms_mvp_tutorial.Models
         {
 
         }
+
+        // Properties
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50]
+        public string RoleName { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
     }
 }
