a695d9a82f556432ba0f20746295a5567ccfd33f
Edgar Eldy <ed.eldy21@live.com>
2021-08-29 19:59:36 +0200

    Add ApplicationDbContext class representing all models

---
+++ winforms_mvp_tutorial/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace winforms_mvp_tutorial.Data
+{
+    class ApplicationDbContext : DbContext
+    {
+    }
+}
