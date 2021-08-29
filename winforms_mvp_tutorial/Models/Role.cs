dcd62e8aa1119fa369eafd00755df1b1e75a36c9
Edgar Eldy <ed.eldy21@live.com>
2021-08-29 20:34:40 +0200

    Add Role Model

---
+++ winforms_mvp_tutorial/Models/Role.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace winforms_mvp_tutorial.Models
+{
+    [Table("roles")]
+    class Role
+    {
+        // Constructor
+        public Role()
+        {
+
+        }
+    }
+}
